d63a5a5f20f29a8f9f691adeb5a087fa482acdbf
marilaks <90045250+marilaks@users.noreply.github.com>
2021-09-04 20:36:00 +0530

    Add files via upload

---
+++ 4.cpp
@@ -0,0 +1,15 @@
+//defining function
+#include <iostream>
+using namespace std;
+
+void fun1()
+{
+	cout<<"In function1"<<endl;
+}
+
+int main()
+{
+	cout<<"starting"<<endl;
+	fun1();
+	cout<<"ending"<<endl;
+}
---
+++ 5.cpp
@@ -0,0 +1,14 @@
+//function parameteres
+#include <iostream>
+using namespace std;
+
+int add(int x, int y){
+	return x+y;
+}
+int main(){
+	
+	cin >> "enter value of a";
+	cin >> "enter value os b";
+	cout << "the value is"<< add(a,b);
+	return 0;
+}
