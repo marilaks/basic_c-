7bdc8ece3056c2729db351d6ba3ac9c432e3b20c
marilaks <90045250+marilaks@users.noreply.github.com>
2021-09-06 22:49:10 +0530

    Add files via upload

---
+++ 11.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+using namespace std;
+
+int display(int &a){
+	a = a+1;
+	return a;
+}
+main(){
+	int a =5;
+	//value before call by reference
+	cout<<"before\n"<<a<<endl;
+	display(a);
+	//value after call by reference
+	cout<<"after\n"<<a;
+}
+/*used when we go for sorting like bubble sort where we have to work with the original 
+data not a copy of it*/
