90ad13561d85d86bdb3d6fa7a31b5d086c246cf7
marilaks <90045250+marilaks@users.noreply.github.com>
2021-09-11 19:21:41 +0530

    Update 3_2_class.cpp

--- basics/3_2_class.cpp
+++ basics/3_2_class.cpp
@@ -1,3 +1,17 @@
+/*
+	An object in C++ as, “a piece of memory that can be used to store values”. An object with a name is called a variable.
+	An object in C++ as, “a piece of memory that can be used to store values”. An object with a name is called a variable.
+EX:
+you.driveTo(work);
+makes it clearer who the subject is (you) and what behavior is being invoked (driving somewhere).
+ Rather than being focused on writing functions, we’re focused on defining objects that have a well-defined set of behaviors.
+  This is why the paradigm is called “object-oriented”.
+ Rather than being focused on writing functions, we’re focused on defining objects that have a well-defined set of behaviors.
+  This is why the paradigm is called “object-oriented”.
+  
+  provides a higher degree of code-reusability. 
+  These objects provide  info about our data by allowing us to define how we interact with the objects, and how they interact with other objects.*/
+
 //class
   #include <iostream>
   using namespace std;
