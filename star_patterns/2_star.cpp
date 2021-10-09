4f511e550aa3240124b7155d1e8982d7069cd401
marilaks <90045250+marilaks@users.noreply.github.com>
2021-10-09 10:48:31 +0530

    Add files via upload

---
+++ star_patterns/2_star.cpp
@@ -0,0 +1,28 @@
+/*
+
+    *
+  * *
+* * *
+
+*/
+
+#include<iostream>
+using namespace std;
+
+int main(){
+	int i,j,n;
+	cout<<"enter rows";
+	cin>>n;
+
+    for(i=1;i<=n;i++)
+    {
+        for(j=1;j<=n-i;j++)
+            cout<<" ";
+            for(j=1;j<=i;j++)
+            {
+                cout<<"*";
+            }
+            cout<<"\n";
+    }
+    return 0;
+}
