e16271cc292f121485729bf1e1f61960da3e5152
Mareeswari Lakshmi <90045250+marilaks@users.noreply.github.com>
2021-10-09 11:20:33 +0530

    Update 3_star.cpp

--- star_patterns/3_star.cpp
+++ star_patterns/3_star.cpp
@@ -11,15 +11,14 @@ int main(){
 	int i,j,n;
 	cout<<"enter rows";
 	cin>>n;
-	char c[] = "ABCD";
-
+	
     for(i=1;i<=n;i++)
     {
         for(j=1;j<=n-i;j++)
             cout<<" ";
             for(j=1;j<=i;j++)
             {
-                cout<<c[i-1];
+                 cout<<((char)(j+64));
             }
             cout<<"\n";
     }
