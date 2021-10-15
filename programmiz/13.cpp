5c3d77667857414e7ffb227510d260ec3ffc1fa4
Mareeswari Lakshmi <90045250+marilaks@users.noreply.github.com>
2021-10-15 10:13:02 +0530

    Add files via upload

---
+++ programmiz/13.cpp
@@ -0,0 +1,17 @@
+//multiplication table
+# include<iostream>
+using namespace std;
+
+int main(){
+	int n,i;
+	
+	cout<<"enter n"<<endl;
+	cin>>n;
+	
+	//multiplication table
+	for(i=1;i<=20;i++){
+		cout<<n<<" * "<<i<<"= "<<n*i<<endl; 
+		
+	}
+	
+}
