b7e5e6c348dc630ce593f56150a0597dac8df4d7
Mareeswari Lakshmi <90045250+marilaks@users.noreply.github.com>
2021-10-17 10:32:27 +0530

    Add files via upload

---
+++ programmiz/finding_elements_array.cpp
@@ -0,0 +1,30 @@
+//finding elements in an array
+#include<iostream>
+using namespace std;
+
+int main(){
+	
+	int n[6],key;
+	cout<<"enter the elements"<<endl;
+	//getting array from user
+	for(int i=0;i<6;i++){
+		cin>>n[i];
+	}
+	
+	//getting key element
+	cout<<"enter the key element"<<endl;
+	cin>>key;
+	
+	//searching the key element
+	for(int j=n[0];j<6;j++){
+		
+		if(key == n[6]){
+			cout<<"element found"<<endl;
+		}
+		else{
+			
+		cout<<"element not found"<<endl;
+	}
+	}
+
+}
