04638efd82cb1eff40311a2531122fbe09329fd6
Mareeswari Lakshmi <90045250+marilaks@users.noreply.github.com>
2021-10-22 09:42:23 +0530

    Update x_pattern.cpp

--- star_patterns/x_pattern.cpp
+++ star_patterns/x_pattern.cpp
@@ -1,4 +1,4 @@
-/*P		P
+/*P     P
    R   R
    O  O
     G
@@ -10,17 +10,17 @@ M        M*/
 using namespace std;
 
 int main(){
-	int rows = 5,space,i,j;
+	int rows = 7,space,i,j;
 	
 	//getting input from user
- char name[] = "names";
+ char name[] = "program";
 	
 	//rows
 	for(i = 0;i<rows;i++){
 		for(j=0;j<rows;j++){
-			if((i == j) || ((i+j) == (rows - 1)){
+			if((i == j) || ((i+j) == (rows - 1))){
 			
-			cout<<name[j];
+			cout<<name[i];
 		}
 		else{
 			cout<<" ";
