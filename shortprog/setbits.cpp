a48d01fe7120f155fac0882dcb91e3378b26b7b5
Mareeswari Lakshmi <90045250+marilaks@users.noreply.github.com>
2021-10-23 19:15:02 +0530

    Add files via upload

---
+++ shortprog/setbits.cpp
@@ -0,0 +1,23 @@
+#include<iostream>
+using namespace std;
+ 
+/* Function to get no of set bits in binary
+representation of positive integer n */
+unsigned int countSetBits(unsigned int n)
+{
+    unsigned int count = 0;
+    while (n) {
+        count += n & 1; //mod by 2
+        n >>= 1; //divide by 2
+    }
+    return count;
+}
+ 
+/* Program to test function countSetBits */
+int main()
+{
+    int i = 1234;
+    cout << countSetBits(i);
+    return 0;
+}
+ 
